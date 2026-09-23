5e43404bdc3d570bced705362770cd5e7d9b9776
Siva Reddy <yennegivenkatasivareddy44@gmail.com>
2026-09-23 17:42:52 +0530

    2nd upd

--- day-5-state-lock/main.tf
+++ day-5-state-lock/main.tf
@@ -9,3 +9,12 @@ resource "aws_subnet" "dev" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
 }
+
+resource "aws_instance" "web" {
+    ami = "ami-0fef201115eefe936"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.dev.id
+    tags = {
+      Name = "web_instance"
+    }
+}
