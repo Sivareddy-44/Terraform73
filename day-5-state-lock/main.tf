529f102641852dd429dff3633f19c4e0066a2794
Siva Reddy <yennegivenkatasivareddy44@gmail.com>
2026-09-23 18:11:40 +0530

    3rd upd

--- day-5-state-lock/main.tf
+++ day-5-state-lock/main.tf
@@ -18,3 +18,15 @@ resource "aws_instance" "web" {
       Name = "web_instance"
     }
 }
+resource "aws_security_group" "web_sg" {
+    name = "web_sg"
+    description = "Allow HTTP and SSH traffic"
+    vpc_id = aws_vpc.name.id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
