fb09dad9c73f82f77492f4c311dbabc1ed8bbc74
Siva Reddy <yennegivenkatasivareddy44@gmail.com>
2026-09-23 18:41:20 +0530

    4th upd

--- day-5-state-lock/backend.tf
+++ day-5-state-lock/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "statefile-store-nit"
     key = "terraform-statefile/terraformtfstate"
     region = "us-east-1"
-    use_lockfile = true    #supports terraform latest version >=1.10
+    #use_lockfile = true    #supports terraform latest version >=1.10
     dynamodb_table = "terraform-statefile-locking"
   }
 }
